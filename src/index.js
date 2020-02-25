17bb6758e0a325853a142f29a71667feb32ae234
Julio Martins de Sousa <jmasousa@voegol.com.br>
2020-02-25 19:51:12 -0300

    INITIAL COMMIT

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+import Reader from './screens/Reader';
+
+export default function App() {
+	return <Reader />;
+}
---
+++ src/screens/Reader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Reader() {
+	return (
+		<SafeAreaView>
+			<Text>Reader</Text>
+		</SafeAreaView>
+	);
+}
